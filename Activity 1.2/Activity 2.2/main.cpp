e88e3440ba91fb8ccbf0cf249c94ca9b49e865a2
Diego Velázquez Moreno <diegoa1962@hotmail.com>
2021-08-27 22:32:28 -0500

    Actualization functional greedy

--- Activity 1.2/Activity 2.2/main.cpp
+++ Activity 1.2/Activity 2.2/main.cpp
@@ -9,9 +9,121 @@
 using namespace std;
 
 int totalCoins[0];
+int change=0;
+
+void merge(int a[], int start, int end)
+{
+    int middle = (start + end) / 2;
+    int j = start;
+    int k = middle + 1;
+    int arraySize = end - start + 1;
+    int arrayTemp[arraySize];
+
+    for(int i=0; i < arraySize; i++)
+    {
+        if(j <= middle && k <= end)
+        {
+            if(a[j] > a[k])
+            {
+                arrayTemp[i] = a[j++];
+            }
+            else
+            {
+                arrayTemp[i] = a[k++];
+            }
+        }
+        else if(j <= middle)
+        {
+            arrayTemp[i] = a[j++];
+        }
+        else
+        {
+            arrayTemp[i] = a[k++];
+        }
+    }
+
+    for(int m=0; m < arraySize; m++)
+    {
+        a[start + m] = arrayTemp[m];
+    }
+}
+
+void mergeSort(int a[], int start, int end)
+{
+    if(start < end)
+    {
+        int middle = (start + end) / 2;
+        mergeSort(a, start, middle);
+        mergeSort(a, middle + 1, end);
+        merge(a, start, end);
+    }
+}
+
+void mergeSortStart(int a[], int size)
+{
+    mergeSort(a, 0, size - 1);
+
+    std::cout << "\nSorted list from highest to lowest of the " << size << " given numbers:\n";
+
+    for (int i=0; i<size; i++)
+    {
+        if(i == size - 1)
+        {
+            std::cout << a[i] << std::endl;
+        }
+        else
+        {
+            std::cout << a[i] << ", ";
+        }
+    }
+}
+ 
+
+
+
+
+bool changeConverter(int cost, int payment) {
+     change=payment-cost;;
+    
+    if(payment<cost){
+        change= change * -1;
+        cout<<"There is not enough money, please add : "<<(change)<<"$"<<endl;
+        return false;
+    }
+    
+    else if(payment==cost){
+        cout<<"Thank you for paying"<<endl;
+        return false;
+    }
+    
+    return true;
+};
+
+int minCoinDynamic(int amount, int coins[], int size, int C[], int s[])
+{
+    C[0] = 0;
+    for(int j = 1; j <= amount; j++) {
+    C[j] = INT_MAX;
+    for(int i = 0; i < size; i++) {
+
+        if(j >= coins[i] && 1 + C[j-coins[i]] < C[j] ) {
+        C[j] = 1 + C[j-coins[i]];
+
+        // i-th denomination used for the amount of j
+        s[j] = i;
+        }
+    }
+    }
+    return C[amount];
+}
+
+
+
+
+
 
 /*
- Function that counts the coins and returns the numbers of coins needed.
+ Function that counts the coins and returns the numbers of coins needed but greedy.
  INPUT
  coins[]= The coins available to choose.
  size = The size of the numbers it can take.
@@ -19,96 +131,108 @@ int totalCoins[0];
  Output = Total of numbers.
 */
 
-int minCoins(int coins[], int total_coins, int N) // Function to return the minimum // coins needed
-{
-        if (N == 0) // If we have a combination then
-        return 0;
-  
-    int result = INT_MAX; // Currently result is initialised as INT_MAX
-  
-    for (int i = 0; i < total_coins; i++) // run until availability of coins
-    {
-        if (coins[i] <= N) { // Add to the list of counting
-            int sub_res = 1 + minCoins(coins, total_coins, N - coins[i]); // add 1 due to the coin inclusion
-            // see if result can minimize
-            if (sub_res < result)
-                result = sub_res;
-        }
-    }
-    return result;
-}  
 
 
-int coinCounter(int coins[], int size, int cost, int payment){
+
+int minCoinGreedy(int coins[], int size, int cost){
     int count = 0;
-    int change=payment-cost;
     
-    if(payment<cost){
-        change= change * -1;
-        cout<<"There is not enough money, please add : "<<(change)<<"$"<<endl;
-    }
-    
-    else if(payment==cost){
-        cout<<"Thank you for paying"<<endl;
-    }
-    
-    
-    else{
-        for(int i = 0; i<size; i++){
-            while (change >= coins[i]){
-                change -= coins[i];
-                totalCoins[count]=coins[i];
-                count++;
-            }
-            if(change == 0){
-                break;
-            }
-            else if(change<coins[size-1]){
-                cout<<"We owe you : "<<change<<"$"<<endl;
-                break;
-            }
+    for(int i = 0; i<size; i++){
+        while (cost >= coins[i]){
+            cost -= coins[i];
+            totalCoins[count]=coins[i];
+            count++;
             
+        }
+        if(cost == 0){
+            break;
+    
+        }
+
             
         }
         return count;
-
-    }
-    
-    
-    
-    return count;
-    
-        
-
-    
     
 }
 
 
 int main() {
-/*    int coins[]={25,20,10,5};
+/*   int coins[]={25,20,10,5};
     int cost = 100;
     int payment = 140;
 
     int size = sizeof(coins)/sizeof(coins[0]);
-    int MinCount = coinCounter(coins,size,cost,payment);
+    int MinCount = coinCounter(coins,size,cost);
     cout<<"Coins : " <<MinCount<<"\n";
     printf("The coins to give are: ");
     for(int i = 0; i < MinCount; i++){
         cout<<endl<<totalCoins[i]<<"$";
 //            printf("%d ", totalCoins[i],"/n");
  
- 
- 
     }
-    cout<<"\n";*/
+
     
-    int coins[] = { 10 ,25, 5 };
-    int sum = 30; // the money to convert
-    int total_coins = 3; // total availability of coins
-    cout << "Minmum coins needed are " << minCoins(coins, total_coins, sum);
+    */
+    
+    int cost,num,payment;
+    int numberArray;
+    cout<<"How many coins do you want to add?"<<endl;
+    cin>> numberArray;
+    cout<<"Enter the coins"<<endl;
+    int coins[numberArray];
+    for(int i=0;i<numberArray;i++){
+        cin>>num;
+        coins[i]=num;
+    }
+    cout<<"Enter the price of the product"<<endl;
+    cin>>cost;
+    cout<<"Enter the amount of money that you are going to pay"<<endl;
+    cin>>payment;
     
     
+    bool flag=changeConverter(cost,payment);
+    
+    
+    int amount = change;
+    int size = sizeof(coins)/sizeof(coins[0]);
+    int *C = new int[amount+1];
+    int *s = new int[amount+1];
+    
+    
+    
+    
+    if(flag == true){
+        
+      
+//      Dynamic algoritm
+        int totalChangeDyn = minCoinDynamic(amount, coins, size, C, s);
+        cout << "The coins to give are = " << totalChangeDyn << endl;
+        cout << "coins used: " ;
+        int k = amount;
+        int sortCoinDyn[totalChangeDyn];
+        int i=0;
+        while(k) {
+            sortCoinDyn[i] = coins[s[k]];
+            k = k - coins[s[k]];
+            i++;
+        }
+        mergeSortStart(sortCoinDyn, totalChangeDyn);
+        
+        
+//      Greedy Algoritm
+        int totalChangeGreedy = minCoinGreedy(coins,size,amount);
+        int sortCoinGreed[totalChangeGreedy];
+        cout<<"Coins : " <<totalChangeGreedy<<"\n";
+        cout << "The coins to give are = " << totalChangeGreedy << endl;
+        for(int i = 0; i < totalChangeGreedy ;i++){
+            sortCoinGreed[i]=totalCoins[i];
+        }
+        mergeSortStart(sortCoinGreed, totalChangeGreedy);
+
+        delete[] C;
+        delete[] s;
+    }
+         
     
     return 0;
 }
