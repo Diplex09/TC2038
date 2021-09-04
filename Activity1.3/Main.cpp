4e6dafcb3262b527050e540e86eef8f2d9ae6003
AldoDeg <aldodegollado@hotmail.com>
2021-09-03 23:56:51 -0500

    Final version of Activity 1.3
    Uploaded the final version of the backtracking and branch and bound algorithms using C++.

--- Activity1.3/Main.cpp
+++ Activity1.3/Main.cpp
@@ -1,153 +1,218 @@
-//
-//  main.cpp
-//  BackTracking
-//
-//  Created by Aldo Degollado Diego Velázquez on 03/09/21.
-//
+/*                                                                                    */
+/*  Actividad 1.3 - Implimentación de algoritmo backtracking y branch and bound       */
+/*                                                                                    */
+/*  Diego Velázquez Moreno - A01632240                                                */
+/*  Aldo Alejandro Degollado Padilla - A01638391                                      */
+/*                                                                                    */
 
 
 #include <stdio.h>
 #include <iostream>
+#include <vector>
+#include <queue>
+
 using namespace std;
 
-
-#include <iostream>
-#include <vector>
-
-// Backtracking
-bool solveMazeBacktracking(int row, int column, vector<vector<bool>> &v);
-bool solveMaze(int row, int column, vector<vector<bool>> &v, int x, int y, vector<vector<bool>> &s);
-
-
-
-
-
-// Función para comprobar que si x,y es un índice válido
-//
-bool isValid(int row, int column, vector<vector<bool>> &maze, int x, int y){
-    if (x >= 0 && x < row && y >= 0 && y < column && maze[x][y] == 1){
-        return true;
-    }
- 
-    return false;
-}
-
-
-// Regresa true en caso de que exista un camino posible,
-// caso contrario regresa false
-bool solveMazeBacktracking(int row, int column, vector<vector<bool>> &maze) {
-    
-    vector<vector<bool>> visited(row,vector<bool>(column,false));
-    
-
-    if (solveMaze(row, column, maze, 0, 0, visited) == false) {
-        cout << "Don't have a solution" << endl;
-        return false;
-    }
- 
-    
-    cout << "Solucion" << endl;
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < column; j++){
-            cout << visited[i][j] << " ";
+// Prints the solution for the maze founded by the algorithm.
+void printMazeSolution(vector<vector<int>>& mazeSolution)
+{
+    for(int i = 0; i < mazeSolution.size(); i++)
+    {
+        for(int j = 0; j < mazeSolution[0].size(); j++)
+        {
+            cout << mazeSolution[i][j] << " ";
         }
         cout << endl;
     }
-
-    return true;
 }
- 
-// Función recursiva para resolver el problema del laberinto
-bool solveMaze(int row, int column, vector<vector<bool>> &maze, int x, int y, vector<vector<bool>> &visited)
+
+// This function verifies if the given coordinate it's inside the maze and it's accesible.
+bool isAccesible(vector<vector<int>> maze, int x, int y)
 {
-    // Si se llegó al final regresa true
-    if (x == row - 1 && y == column - 1 && maze[x][y] == 1) {
-        visited[x][y] = 1;
+    if(x >= 0 && y >= 0 && x < maze.size() && y < maze[0].size() && maze[x][y])
+    {
         return true;
     }
- 
-    // Comprueba si maze[x][y] es válido
-    if (isValid(row, column, maze, x, y) == true) {
-        // Comprueba si la posición actual ya forma parte
-        // de la solución
-        if (visited[x][y] == 1){
-            return false;
-        }
-       
-        // Marca la posición como parte de la solución
-        visited[x][y] = 1;
-        
-
-        // Se mueve hacia abajo en la posición y
-        if (solveMaze(row, column, maze, x, y + 1, visited)== true) {
-            return true;
-        }
-        // Si moverse en la posición y no da resultado
-        // Se avanza en la posición x
-        if (solveMaze(row, column, maze, x + 1, y, visited) == true){
-            return true;
-        }
- 
-
-       
-        // Si moverse en la posición y no da resultado
-        // Se regresa en la posición x
-        if (solveMaze(row, column, maze, x - 1, y, visited) == true) {
-            return true;
-        }
- 
-        // Si regresar en la posición x no da resultado
-        // Se mueve hacia arriba en la posición y
-        if (solveMaze(row, column, maze, x, y - 1, visited) == true) {
-            return true;
-        }
- 
-
-        // Si ninguno de los anteriores movimientos
-        // da resultado, se desmarca la posición actual
-        // como parte de la solución
-        visited[x][y] = 0;
-        return false;
-    }
- 
     return false;
 }
 
-int main() {
-    int row, column;
+// Recursive function that goes through the maze until it finds a solution or exhausts all positionsibilities.
+bool solveMazeBacktrackRecursive(vector<vector<int>> maze, vector<vector<int>>& mazeSolution, int x, int y)
+{
+    // Verifies if the end of the maze was reached.
+    if(x == maze.size() - 1 && y == maze[0].size() - 1)
+    {
+        mazeSolution[x][y] = 1;
+        return true;
+    }
 
-    cout << "Type row and columns: " << ::endl;
-    cin >> row;
-    cin >> column;
+    // Verifies if the current positionition in the maze is part of the solution.
+    if(isAccesible(maze, x, y)) 
+    {
+        if(mazeSolution[x][y]) 
+        {
+            return false;
+        }
 
-    vector<vector<bool>> maze;
+        mazeSolution[x][y] = 1;
+        
+        if(solveMazeBacktrackRecursive(maze, mazeSolution, x + 1, y))
+        {
+            return true;
+        }
+
+        if(solveMazeBacktrackRecursive(maze, mazeSolution, x, y + 1))
+        {
+            return true;
+        }
+
+        if(solveMazeBacktrackRecursive(maze, mazeSolution, x - 1, y))
+        {
+            return true;
+        }
+
+        if(solveMazeBacktrackRecursive(maze, mazeSolution, x, y - 1))
+        {
+            return true;
+        }
+
+        mazeSolution[x][y] = 0;
+        return false;
+    }
+    return false;
+}
+
+// This function initializes the process of searching for a solution for the maze using backtracking method.
+void solveMazeBacktrack(vector<vector<int>> maze, vector<vector<int>>& mazeSolution)
+{
+    int m = maze.size();
+    int n = maze[0].size();
+
+    if(!maze[0][0])
+    {
+        cout << "The maze has no entrance." << endl;
+        return;
+    }
+
+    if(!maze[m-1][n-1])
+    {
+        cout << "The maze has no exit." << endl;
+        return;
+    }
+
+    if(!solveMazeBacktrackRecursive(maze, mazeSolution, 0, 0))
+    {
+        cout << "The maze has no solution." << endl;
+        return;
+    }
+}
+
+// This function uses the branch and bound method (BFS algorithm) to find a solution for the maze.
+void solveMazeBranchBound(vector<vector<int>> maze, vector<vector<int>>& mazeSolution)
+{
+    int x = maze.size();
+    int y = maze[0].size();
+
+    if(!maze[0][0])
+    {
+        cout << "The maze has no entrance." << endl;
+        return;
+    }
+
+    if(!maze[x-1][y-1])
+    {
+        cout << "The maze has no exit." << endl;
+        return;
+    }
+
+    vector<vector<bool>> current(x, vector<bool>(y, false));
+    queue<vector<int>> mazeQueue;
+
+    int xNum[] = {1, 0, 0 , -1};
+    int yNum[] = {0, 1, -1, 0};
     
-    
-    bool path;
-    cout << "Type the maze " << endl;
-    for (int i = 0; i < row; i++) {
-        int j = 0;
-        vector<bool> line;
-        while(j < column){
-            cin >> path;
-            line.push_back(path);
-            j++;
-            if (cin.peek() == '\n'){
-                break;
+    int row = 0;
+    int col = 0;
+    int stride = 0;
+    std::vector<int> position = {row, col, stride};
+
+    mazeQueue.push(position);
+    current[row][col] = true;
+
+    while(!mazeQueue.empty())
+    {
+        position = mazeQueue.front();
+
+        if(position[2] < stride)
+        {
+            row = position[0];
+            col = position[1];
+            stride = position[2];
+        }
+
+        if(row == maze.size() - 1 && col == maze[0].size()-1)
+        {
+            mazeSolution[row][col] = 1;
+            return;
+        }
+
+        mazeQueue.pop();
+
+        mazeSolution[row][col] = 1;
+
+        for(int i = 0; i < maze.size(); i++)
+        {
+            int newRow = row + xNum[i];
+            int newColumn = col + yNum[i];
+
+            if(isAccesible(maze, newRow, newColumn) && !current[newRow][newColumn])
+            {
+                current[newRow][newColumn] = true;
+                mazeQueue.push({newRow, newColumn, stride++});
             }
+        }
+    }
+    cout << "The maze has no solution." << endl;
+}
+
+int main()
+{
+    cout << "\n |-|-|      BACKTRACKING AND BRANCH AND BOUND IMPLEMENTATION      |-|-|" << endl;
+    cout << "By: Aldo Degollado, Diego Velazquez\n" << endl;
+
+    int m;
+    int n;
+
+    vector<vector<int>> maze;
+
+    cout << "Type the amount of rows and columns: " << endl;
+    cin >> m;
+    cin >> n;
+
+    cout << "\nInsert the maze: " << endl;
+    for(int i = 0; i < m; i++)
+    {
+        vector<int> line;
+        for(int j = 0; j < n; j++)
+        {
+            int o;
+            cin >> o;
+            line.push_back(o);
         }
         maze.push_back(line);
     }
-    cout << "Backtracking" << endl;
-    solveMazeBacktracking(row, column, maze);
+
+    cout << "\nBacktracking aproach: " << endl;
+    vector<vector<int>> backtracking(m, vector<int>(n, 0));
+    solveMazeBacktrack(maze, backtracking);
+    printMazeSolution(backtracking);
+
+    cout << endl;
+
+    cout << "Branch and bound aproach: " << endl;
+    vector<vector<int>> branchAndBound(m, vector<int>(n, 0));
+    solveMazeBranchBound(maze, branchAndBound);
+    printMazeSolution(branchAndBound);
 
     return 0;
 }
-
-
-    
-    
-
-
-    
-
